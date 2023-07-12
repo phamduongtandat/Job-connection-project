f435a8189461f45b886a0036bec68bfdeb7c0281
phvietanh1998 <58402497+phvietanh1998@users.noreply.github.com>
2023-07-12 21:03:45 +0700

    fix UI alljob

--- client/src/components/layout/JobTab.jsx
+++ client/src/components/layout/JobTab.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
-import useGetAuthInfo from "./../../hooks/useGetAuthInfo";
-import { useEffect } from "react";
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import useGetAuthInfo from './../../hooks/useGetAuthInfo';
+import { useEffect } from 'react';
 
 function JobTab({ children, tab1, tab2, navi, button, setIsSwitch, isSwitch }) {
   const { user } = useGetAuthInfo();
@@ -14,9 +14,9 @@ function JobTab({ children, tab1, tab2, navi, button, setIsSwitch, isSwitch }) {
   }, []);
 
   return (
-    <div className="mx-28">
+    <div className="mx-auto max-w-7xl">
       <div className="  my-7  text-sm font-medium text-center text-purple-600 border-b  border-yellow-300 ">
-        <ul className="flex -mb-px  ">
+        <ul className="grid grid-cols-2 -mb-px  ">
           <li>
             {button ? (
               <button
@@ -25,8 +25,8 @@ function JobTab({ children, tab1, tab2, navi, button, setIsSwitch, isSwitch }) {
                 }}
                 className={`block hover:border-b-solid hover:border-r-0 hover:border-dashed hover:border hover:border-b-white hover:border-yellow-400 text-xl w-[600px]  py-4 font-bold leading-10 rounded-t-xl ${
                   isSwitch
-                    ? "font-medium border border-b-white  border-yellow-300 hover:border-r"
-                    : "text-gray-400"
+                    ? 'font-medium border border-b-white  border-yellow-300 hover:border-r'
+                    : 'text-gray-400'
                 }`}
               >
                 {tab1}
@@ -37,8 +37,8 @@ function JobTab({ children, tab1, tab2, navi, button, setIsSwitch, isSwitch }) {
                 className={({ isActive }) =>
                   `block hover:border-b-solid hover:border-r-0  hover:border-dashed hover:border hover:border-b-white hover:border-yellow-400 text-xl w-[600px]  py-4 font-bold leading-10 rounded-t-xl ${
                     isActive
-                      ? "font-medium border border-b-white  border-yellow-300 hover:border-r"
-                      : "text-gray-400"
+                      ? 'font-medium border border-b-white  border-yellow-300 hover:border-r'
+                      : 'text-gray-400'
                   }`
                 }
               >
@@ -55,8 +55,8 @@ function JobTab({ children, tab1, tab2, navi, button, setIsSwitch, isSwitch }) {
                 }}
                 className={`block hover:border-b-solid hover:border-l-0 hover:border-dashed hover:border hover:border-b-white hover:border-yellow-400 text-xl w-[600px]  py-4 font-bold leading-10 rounded-t-xl ${
                   isSwitch
-                    ? "text-gray-400"
-                    : "font-medium  border border-b-white  border-yellow-300  hover:border-l"
+                    ? 'text-gray-400'
+                    : 'font-medium  border border-b-white  border-yellow-300  hover:border-l'
                 }`}
               >
                 {tab2}
@@ -67,8 +67,8 @@ function JobTab({ children, tab1, tab2, navi, button, setIsSwitch, isSwitch }) {
                 className={({ isActive }) =>
                   `block hover:border-b-solid hover:border-l-0 hover:border-dashed hover:border hover:border-b-white hover:border-yellow-400 text-xl w-[600px]  py-4 font-bold leading-10 rounded-t-xl ${
                     isActive
-                      ? "font-medium  border border-b-white  border-yellow-300  hover:border-l"
-                      : "text-gray-400"
+                      ? 'font-medium  border border-b-white  border-yellow-300  hover:border-l'
+                      : 'text-gray-400'
                   }`
                 }
               >
